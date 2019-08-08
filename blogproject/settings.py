46a96bb9140bba5c544c48b546f08e2aec378849
surin-0 <marblegiraffe@ajou.ac.kr>
2019-08-08 18:40:36 +0900

    cname

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'egv2vqbwxtbr19qrnay+^@_$-7wu4qpb+_-g-hfnoa3rpn&t26'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [django-env.mw6gsdmcdq.us-west-2.elasticbeanstalk.com]
 
 
 # Application definition
